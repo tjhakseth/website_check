094c055aeccf5653aba12f45a9fb05f1c4681f50
Tiffany Hakseth <tjhakseth@gmail.com>
2016-03-31 13:47:17 -0700

    Website check added

--- server.py
+++ server.py
@@ -12,12 +12,23 @@ Session.configure(bind=engine)
 interval = int(raw_input("Enter number of seconds: "))
 url = raw_input("Enter website: ")
 
+
+def url_check(url):
+    
+    r = requests.head(url)
+    if r.status_code > 400:
+        url = raw_input("Please enter a valid website: ")
+
+
 while True:
     if interval < 1:
         interval = 60
+
     r = requests.get(url)
     HTTP_status_code = r.status_code
     timestamp = r.headers['Date']
+
+    print type(HTTP_status_code)
 
     new_record = Status(HTTP_status_code=HTTP_status_code, url=url, timestamp=timestamp)
     session = Session()
