87995e5fe931058ae5a7734c72a39682e6bbc8b1
Tiffany Hakseth <tjhakseth@gmail.com>
2016-03-31 19:03:05 -0700

    Added README and requirements docs

---
+++ README.md
@@ -0,0 +1,10 @@
+Install requirements.txt
+
+Steps to create Database:
+    Download/Run postgres
+    In command line 
+        $ dropdb lookoutdb
+        $ createdb lookoutdb
+
+
+    
--- server.py
+++ server.py
@@ -1,41 +1,104 @@
 """Lookout Backend Internship - Services/Security Homework"""
+import argparse
 import requests
+import signal
 import time
 from sqlalchemy.orm import sessionmaker
 from model import Status, db_connect, create_status_table
 
-engine = db_connect()
-create_status_table(engine)
-Session = sessionmaker()
-Session.configure(bind=engine)
+
+def command_line_parser():
+    """Creates command line string"""
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                       help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                       const=sum, default=max,
+                       help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+
+def db_connection():
+    """Connects to postgres database"""
+
+    engine = db_connect()
+    create_status_table(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+
 
 interval = int(raw_input("Enter number of seconds: "))
 url = raw_input("Enter website: ")
 
 
 def url_check(url):
-    
+    """Checks to make sure the url is valid"""
+
     r = requests.head(url)
-    if r.status_code > 400:
-        url = raw_input("Please enter a valid website: ")
+    if r.status_code != 200:
+        url = raw_input("Error-please enter a valid website: ")
+    else:
+        return url
+
+    # try:
+    #     r = requests.head(url)
+    #     print(r.status_code)
+    #     # prints the int of the status code. Find more at httpstatusrappers.com :)
+    # except requests.ConnectionError:
+    #     print("failed to connect")
 
 
-while True:
-    if interval < 1:
-        interval = 60
+def download_file(url):
+    """Downloads a file and chunks it for better downloading"""
 
-    r = requests.get(url)
-    HTTP_status_code = r.status_code
-    timestamp = r.headers['Date']
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk:# filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush()
+    return local_filename
 
-    print type(HTTP_status_code)
 
-    new_record = Status(HTTP_status_code=HTTP_status_code, url=url, timestamp=timestamp)
-    session = Session()
-    session.add(new_record)
-    session.commit()
+def signal_handler(signum, frame):
+    """Timeout for websites"""
 
-    print HTTP_status_code
-    print url
-    print timestamp
-    time.sleep(interval)
+    raise Exception("Timed out!")
+
+signal.signal(signal.SIGALRM, signal_handler)
+signal.alarm(10)   # Ten seconds
+try:
+    long_function_call()
+except Exception, msg:
+    print "Timed out!"
+
+
+def process_site(interval, url):
+    """Processes the website and adds to the database"""
+    while True:
+        if interval < 1:
+            interval = 60
+
+        r = requests.get(url)
+        HTTP_status_code = r.status_code
+        timestamp = r.headers['Date']
+
+        print type(HTTP_status_code)
+
+        new_record = Status(HTTP_status_code=HTTP_status_code, url=url, timestamp=timestamp)
+        session = Session()
+        session.add(new_record)
+        session.commit()
+
+        print HTTP_status_code
+        print url
+        print timestamp
+        time.sleep(interval)
+
+
+if __name__ == '__main__':
