d81cac91cfdfbd7fe9af083028c4bb886dd82122
Tiffany Hakseth <tjhakseth@gmail.com>
2016-03-31 21:45:31 -0700

    Completed README, refactoring of code, updated database

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-Install requirements.txt
+# Lookout Backend Internship - Services/Security Homework
 
-Steps to create Database:
-    Download/Run postgres
-    In command line 
-        $ dropdb lookoutdb
-        $ createdb lookoutdb
+## Install requirements.txt
 
+`$ pip install -r requirements.txt`
 
-    
+## Steps to create Database:
+ 
+ Download/Run postgres
+ 
+ `$ dropdb lookoutdb`
+ 
+ `$ createdb lookoutdb`
+ 
+## To run the program
+Command Line Syntax:
+
+`$ python sitecheck.py -u <url> -d <database connection string> [-i interval]`
+
+Example:
+
+`$ python sitecheck.py -u http://www.google.com -d postgresql://localhost/lookoutdb -i 2`
+
+For full help:
+
+`$ python sitecheck.py --help`
--- model.py
+++ model.py
@@ -1,24 +1,30 @@
 """Database for Lookout"""
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.engine.url import URL
 
 Base = declarative_base()
 
+
 def db_connect(database):
+    """Connects to database"""
 
     engine = create_engine(database)
 
     return engine
 
+
 def create_status_table(engine):
+    """Create tables in the database"""
 
     Base.metadata.create_all(engine)
 
+
 class Status(Base):
+    """Status Table"""
+
     __tablename__ = 'status'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    HTTP_status_code = Column(String(3), nullable=False)
+    HTTP_status_code = Column(Integer, nullable=False)
     url = Column(String(500), nullable=False)
-    timestamp = Column(String(500), nullable=False)
+    timestamp = Column(DateTime, nullable=False)
